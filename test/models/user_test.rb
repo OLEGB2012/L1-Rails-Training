703a2d87bfd38bf850318eb4fb4c8051b5e6078c
olb <olegb2012@gmail.com>
2021-05-24 12:02:49 +0300

    Implements tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test 'a user with no attributes is not valid' do
+    user = User.new
+    assert_not user.save, 'Saved a user with no attributes.'
+  end
+  test 'an empty user is not valid' do
+    assert !User.new.valid?, 'Saved an empty user.'
+  end
+  test "the two fixture users are valid" do
+    assert User.new(last_name: users(:one).last_name, login_name:
+      users(:one).login_name ).valid?, 'First fixture is not valid.'
+    assert User.new(last_name: users(:two).last_name, login_name:
+      users(:two).login_name ).valid?, 'Second fixture is not valid.'
+  end
+end
